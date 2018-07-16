97860f30806a28720738c485265aa3f77c6c4651
Viktor Ten <viktor.ten@me.com>
2018-07-16 17:32:17 +0600

    Rollback

--- config/initializers/houston.rb
+++ config/initializers/houston.rb
@@ -1,6 +1,6 @@
 require 'houston'
 
-if Rails.env.development? || true
+if Rails.env.development?
   APN = Houston::Client.development
   APN.certificate = File.read(Rails.root.join('config/push_certificates/development_kz.dar.lab.pingoapp.pem'))
 else
